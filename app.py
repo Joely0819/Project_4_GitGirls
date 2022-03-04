5aeb05aa01bb671b070d35a5c563b510fa2c0d81
gtmorganb <gtmorganb@gmail.com>
2022-03-04 16:20:21 -0500

    working flask

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 from flask import *
 from functools import wraps
 import sqlite3
@@ -5,7 +6,7 @@ import sqlite3
 app = Flask(__name__)
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('web_page/index.html')
 
 @app.route('/generate')
 def generate():
